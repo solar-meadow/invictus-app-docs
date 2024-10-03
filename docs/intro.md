9a64998887c5a1da2676def0c9c2f7f3c1dea461
Abdurahman Tanatar <tanatar-abdurahman@mail.ru>
2024-10-04 00:04:12 +0500

    initial

---
+++ docs/intro.md
@@ -0,0 +1,41 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+---
+
+title: How to Download the Имя приложение
+description: Step-by-step guide for downloading the Имя приложение
+
+---
+
+# How to Download the Invictus
+
+This guide will walk you through the steps to download and install the **Имя приложение** on your mobile device.
+
+## Step 1: Visit the App Store
+
+-   **iOS**: Open the App Store on your iPhone or iPad.
+-   **Android**: Open the Google Play Store on your Android device.
+
+## Step 2: Search for Имя приложение
+
+Use the search bar to type in "**Имя приложение**" and tap on the search icon.
+
+## Step 3: Download the App
+
+-   Tap on the download button next to the **Имя приложение** icon.
+-   Wait for the app to install on your device.
+
+## Step 4: Open the App
+
+Once the download is complete, tap the **Open** button to launch the app.
+
+## Step 5: Set Up Your Account
+
+Follow the instructions within the app to set up your account and start using the features.
+
+For more help, refer to our [FAQ](#).
+
+---
---
+++ docusaurus.config.js
@@ -0,0 +1,148 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'Invictus',
+    tagline: 'Сделать фитнес привычкой',
+    favicon: 'img/logo.svg',
+
+    // Set the production url of your site here
+    url: 'https://your-docusaurus-site.example.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'invictus', // Usually your GitHub org/user name.
+    projectName: 'invictusapp', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'ru',
+        locales: ['ru', 'kz'],
+        localeConfigs: {
+            ru: {
+                label: 'Русский',
+            },
+            kz: {
+                label: 'Қазақша',
+            },
+        },
+    },
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: './sidebars.js',
+                    routeBasePath: '/',
+
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    // editUrl:
+                    //'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                blog: false,
+
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'Invictus',
+                logo: {
+                    alt: 'Invictus Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'tutorialSidebar',
+                        position: 'left',
+                        label: 'Demo',
+                    },
+                    {
+                        type: 'localeDropdown',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Demo',
+                        items: [
+                            {
+                                label: 'Tutorial',
+                                to: '/',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Invictus',
+                        items: [
+                            {
+                                label: 'Go',
+                                href: 'https://invictusgo.kz',
+                            },
+                            {
+                                label: 'Girls',
+                                href: 'https://invictusfitness.kz/girls2',
+                            },
+                            {
+                                label: 'Fitness',
+                                href: 'https://invictusfitness.kz/',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Правила и условия',
+                        items: [
+                            {
+                                label: 'Публичная оферта на русском',
+                                href: 'https://docs.google.com/document/d/1RkZaYD2VoYHkptfBpiNVhgQWlmCeFXpN/edit',
+                            },
+                            {
+                                label: 'Публичная оферта на казахском',
+                                href: 'https://docs.google.com/document/d/1mKLB3uF4_78oUt-cfoTKlQ0ooIWRJ2GO/edit',
+                            },
+                            {
+                                label: 'Политика конфедценциальности',
+                                href: 'https://docs.google.com/document/d/155RizJ0qXgIKbQoJf17BIy6T8m0hqIOa/edit',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `© ${new Date().getFullYear()} Invictus`,
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+        }),
+};
+
+export default config;
